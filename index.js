7e2119c087052ab5c17c5fa88d7afadeadd0bede
hermontes <hermontes6@gmail.com>
2024-12-18 16:32:57 -0500

    Remove unnecessary comments

--- index.js
+++ index.js
@@ -1,19 +1,7 @@
 
 function openBurgerMenu() {
 
-  //code changing burger button to close X button
-  // const img = document.getElementById("burger-menu-image");
-  // const imageSrc = img.getAttribute("src");
 
-  // if(imageSrc === "close.png") {
-  //   document.getElementById("burger-menu-image").src = "menu.png"
-
-  // } else {
-  //   document.getElementById("burger-menu-image").src = "close.png"
-
-  // }
-
-  //Show the popout menu once the burger menu is clicked
   const overlayDiv = document.getElementById("close-burger-menu");
   overlayDiv.style.width = "100%";
 
