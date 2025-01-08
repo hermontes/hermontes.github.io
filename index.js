3f759143919da7c312b57dccdaf11470a07810a9
hermontes <hermontes6@gmail.com>
2025-01-08 18:38:30 -0500

    Styling

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
 
 function openBurgerMenu() {
-
-
   const overlayDiv = document.getElementById("close-burger-menu");
   overlayDiv.style.width = "100%";
-
-
-
 }
 
 function closeBurgerMenu() {
   //Close popout menu once X is clicked
   const overlayDiv = document.getElementById("close-burger-menu");
   overlayDiv.style.width = "0";
-
-
-  
 }
