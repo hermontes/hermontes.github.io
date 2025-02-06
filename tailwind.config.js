c36f63fef131f1114583eb5b2cd51779bf031c12
Hermon Tesfatsion <64483255+hermontes@users.noreply.github.com>
2025-02-06 01:26:43 -0500

    Reformat

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  darkMode: 'class', // or 'media' if you prefer auto dark mode based on user preference
+  content: [
+    './src/**/*.{html,js}', // Adjust this if your project structure is different
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
